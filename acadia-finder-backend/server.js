a610b34ab19dd8c5c2e8045d54cb6c91068141fa
Noor Ahmed <noor.ahmed@fundyla.ca>
2021-10-13 22:22:00 -0300

    Created main server and books endpoint

---
+++ acadia-finder-backend/server.js
@@ -0,0 +1,22 @@
+// require('dotenv').config()
+const cors = require("cors")
+const express = require('express')
+const app = express()
+
+app.use(cors())
+
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb://localhost/acadia_finder_db", { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log("Connected to Database"))
+
+app.use(express.json())
+
+// middleware
+const booksRouter = require('./endpoints/books')
+app.use('/books', booksRouter)
+
+
+app.listen(3000, () => console.log("server started"))
